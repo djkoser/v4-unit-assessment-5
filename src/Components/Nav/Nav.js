509c2f717299e02e50ccdd6aa15c838b76f5390f
djkoser <koser.david@gmail.com>
2021-03-26 11:45:59 -0500

    Complete through Redux section

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -4,6 +4,7 @@ import homeLogo from './../../assets/home_logo.png';
 import newLogo from './../../assets/new_logo.png';
 import logoutLogo from './../../assets/shut_down.png';
 import './Nav.css';
+import { Link, withRouter } from 'react-router-dom';
 
 class Nav extends Component {
   constructor(props) {
@@ -19,28 +20,34 @@ class Nav extends Component {
 
   getUser() {
     axios.get('/api/auth/me')
-    .then(res => 'replace this string with something useful')
+      .then(res => 'replace this string with something useful')
   }
-  
+
   logout() {
     axios.post('/api/auth/logout')
       .then(_ => 'replace this string with something else')
   }
-  
+
   render() {
-      return this.props.location.pathname !== '/' &&
-        <div className='nav'>
-          <div className='nav-profile-container'>
-            <div className='nav-profile-pic'></div>
-            <p>placeholder username</p>
-          </div>
-          <div className='nav-links'>
-            <img className='nav-img' src={homeLogo} alt='home' />
-            <img className='nav-img' src={newLogo} alt='new post' />
-          </div>
-          <img className='nav-img logout' src={logoutLogo} alt='logout' />
+    return this.props.location.pathname !== '/' &&
+      <div className='nav'>
+        <div className='nav-profile-container'>
+          <div className='nav-profile-pic'></div>
+          <p>placeholder username</p>
         </div>
+        <div className='nav-links'>
+          <Link to='/dash'>
+            <img className='nav-img' src={homeLogo} alt='home' />
+          </Link>
+          <Link to='/form'>
+            <img className='nav-img' src={newLogo} alt='new post' />
+          </Link>
+        </div>
+        <Link to='/' onClick={()=> this.logout()}>
+          <img className='nav-img logout' src={logoutLogo} alt='logout' />
+        </Link>
+      </div>
   }
 }
 
-export default Nav;
+export default withRouter(Nav);
