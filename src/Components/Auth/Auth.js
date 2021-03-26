753e3786f71149ab6fc36494e4a5927361d6119f
djkoser <koser.david@gmail.com>
2021-03-26 13:08:43 -0500

    Start responsive design

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import logo from './../../assets/helo_logo.png';
 import './Auth.css';
+import { connect } from 'react-redux';
+import { updateUser } from '../../redux/reducer';
 
 class Auth extends Component {
   constructor(props) {
@@ -24,29 +26,33 @@ class Auth extends Component {
   login() {
     axios.post('/api/auth/login', this.state)
       .then(res => {
+        const {profile_pic, username} = res.data
         this.props.history.push('/dash')
+        this.props.updateUser({profilePicture:profile_pic, username});
       })
       .catch(err => {
         console.log(err)
-        this.setState({errorMsg: 'Incorrect username or password!'})
+        this.setState({ errorMsg: 'Incorrect username or password!' })
       })
   }
 
   register() {
     axios.post('/api/auth/register', this.state)
       .then(res => {
-        this.props.history.push('/dash')
+        const {profile_pic, username} = res.data; 
+        this.props.history.push('/dash');
+        this.props.updateUser({profilePicture:profile_pic, username});
       })
       .catch(err => {
         console.log(err)
-        this.setState({errorMsg: 'Username taken!'})
+        this.setState({ errorMsg: 'Username taken!' })
       })
   }
 
   closeErrorMessage = () => {
     this.setState({
-      errorMsg: false, 
-      username: '', 
+      errorMsg: false,
+      username: '',
       password: ''
     })
   }
@@ -76,4 +82,4 @@ class Auth extends Component {
   }
 }
 
-export default Auth;
+export default connect(null, { updateUser })(Auth);
---
+++ src/redux/reducer.js
@@ -0,0 +1,33 @@
+const initialState = {
+  username: "",
+  profilePicture: ""
+}
+
+const UPDATE_USER = "UPDATE_USER";
+const LOGOUT = "LOGOUT"; 
+export const updateUser = (userObject) => {
+  return {
+    type:UPDATE_USER,
+    payload:{...userObject}
+  }
+}
+
+export const logout = () => {
+  return {
+    type:"LOGOUT"
+  }
+}
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case UPDATE_USER:
+      state = {...state,...action.payload}; 
+      return state; 
+    case LOGOUT:
+      state = {username:"", profilePicture:""};
+      return state;
+    default:
+      console.log(`${action.type} is not a valid case`);
+      return state; 
+  }
+}
